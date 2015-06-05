fa67214e74e4b369d610619ce3ab563fb6069993
yangzexin-imac <yangzexin2011@gmail.com>
2015-06-05 10:06:26 +0800

    init

---
+++ MMLuaBridge/MMLuaBridge.h
@@ -0,0 +1,13 @@
+//
+//  MMLuaBridge.h
+//  MMLuaBridge
+//
+//  Created by yangzexin on 6/5/15.
+//  Copyright (c) 2015 yangzexin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMLuaBridge : NSObject
+
+@end
---
+++ MMLuaBridge/MMLuaBridge.m
@@ -0,0 +1,13 @@
+//
+//  MMLuaBridge.m
+//  MMLuaBridge
+//
+//  Created by yangzexin on 6/5/15.
+//  Copyright (c) 2015 yangzexin. All rights reserved.
+//
+
+#import "MMLuaBridge.h"
+
+@implementation MMLuaBridge
+
+@end
